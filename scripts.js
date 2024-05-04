a90345f99f06d05ac895ccf52ca577724e7687a5
Marcio Silva <silvamc83@gmail.com>
2024-05-04 08:18:15 -0300

    projeto inicial

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+
+const key = "d1ab6ef8dab87b13713651e21d2e780b"
+
+function colocarNaTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°c"
+    document.querySelector("texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector("img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+
+}
